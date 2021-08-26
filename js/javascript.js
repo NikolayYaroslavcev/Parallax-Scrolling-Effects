01de64388a5f99268faeaf4d34bdf3dc7978bbd0
Николай <80519843+NikolayYaroslavcev@users.noreply.github.com>
2021-08-26 13:25:34 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-let start = document.getElementById('start');
+let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
@@ -8,7 +8,7 @@ let header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    start.style.left = value * 0.25 + 'px';
+    stars.style.left = value * 0.25 + 'px';
     moon.style.top = value * 1.05 + 'px';
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
